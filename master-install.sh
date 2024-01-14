937186853b72327469048dd87bcfe1eaf7c3eef8
Oleg Tsvinev <otsvinev@bitglass.com>
2024-01-14 12:27:34 -0800

    Added master-install.sh script and usa case

--- master-install.sh
+++ master-install.sh
@@ -6,13 +6,18 @@ K_LOCAL_HOST_IP=$(hostname -I | cut -d' ' -f1) # or more reliable: ip -4 -o addr
 K_MASTER_IP=$K_LOCAL_HOST_IP
 K_TLS_SANS="--tls-san=$K_MASTER_IP --tls-san=$K_LOCAL_HOST_NAME --tls-san=rpi003.h.remmirath.com"
 K_INSTALL_SCRIPT=./k3s-install.sh
-curl https://get.k3s.io -o $K_INSTALL_SCRIPT
+curl --no-progress-meter https://get.k3s.io -o $K_INSTALL_SCRIPT
 chmod +x $K_INSTALL_SCRIPT
 
 export INSTALL_K3S_VERSION=1.27.9
 
+COMMAND='--disable=traefik --flannel-backend=host-gw'
 for var in "${!K_@}"; do
   COMMAND+=("${!var}")
 done
+COMMAND+='--cluster-init'
 
 echo "${COMMAND[@]}"
+
+#$K_INSTALL_SCRIPT --disable=traefik --flannel-backend=host-gw $K_TLS_SANS  --bind-address=$K_MASTER_IP --advertise-address=$K_MASTER_IP --node-ip=$K_MASTER_IP --cluster-init
+
