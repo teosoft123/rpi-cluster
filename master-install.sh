cce105dd7d503ef1a39dcf212634443c667570b9
Oleg Tsvinev <otsvinev@bitglass.com>
2024-01-14 12:08:12 -0800

    Added master-install.sh script

---
+++ master-install.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+K_LOCAL_HOST_NAME=$(hostname)
+K_LOCAL_HOST_IP=$(hostname -I | cut -d' ' -f1) # or more reliable: ip -4 -o addr show eth0 | awk '{print $4}' | cut -d'/' -f1
+
+K_MASTER_IP=$K_LOCAL_HOST_IP
+K_TLS_SANS="--tls-san=$K_MASTER_IP --tls-san=$K_LOCAL_HOST_NAME --tls-san=rpi003.h.remmirath.com"
+K_INSTALL_SCRIPT=./k3s-install.sh
+curl https://get.k3s.io -o $K_INSTALL_SCRIPT
+chmod +x $K_INSTALL_SCRIPT
+
+export INSTALL_K3S_VERSION=1.27.9
+
+for var in "${!K_@}"; do
+  COMMAND+=("${!var}")
+done
+
+echo "${COMMAND[@]}"
