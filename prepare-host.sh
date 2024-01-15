9c0c127f1473174efc8c6a94b09a4c8e67b3bbcd
Oleg Tsvinev <otsvinev@bitglass.com>
2024-01-14 17:47:30 -0800

    Scripted reqired groups to /boot/cmdline.txt

--- prepare-host.sh
+++ prepare-host.sh
@@ -9,10 +9,17 @@ sudo apt install smartmontools vim -y
 # turn the swap off immediately and permanently
 sudo swapoff -a
 sudo sed -iE 's/CONF_SWAPSIZE=.*/CONF_SWAPSIZE=0/g' /etc/dphys-swapfile
-# enable required cgroups if they're not already present
-CGROUPS_PRESENT=$(sudo cat /boot/cmdline.txt | grep -iEo "cgroup_enable\s*=\s*cpuset|cgroup_memory\s*=\s*1|cgroup_enable\s*=\s*memory")
-if [ "" == "$CGROUPS_PRESENT" ]; then
-  printf "One of required cgroups present, cannot continue: %s\n" "$CGROUPS_PRESENT"
+
+# enable required cgroups only if they're not already present
+BOOTFILE=/boot/cmdline.txt
+REQUIRED_GROUPS="cgroup_enable=cpuset cgroup_memory=1 cgroup_enable=memory"
+ALL_CGROUPS_PRESENT=$(sudo cat $BOOTFILE |  grep -iE 'cgroup_enable\s*=\s*cpuset' | grep -iE 'cgroup_memory\s*=\s*1' | grep 'cgroup_enable\s*=\s*memory')
+if [ "" != "$ALL_CGROUPS_PRESENT" ]; then
+  printf "All of required cgroups present, not making any changes: %s\n" "$ALL_CGROUPS_PRESENT"
+else
+  # adding required groups
+  printf "%s\n" "Adding required groups: ${REQUIRED_GROUPS}"
+  sudo sed -i "s/$/ ${REQUIRED_GROUPS}/" $BOOTFILE
 fi
 
-#sudo vi /etc/dphys-swapfile -> set CONF_SWAPSIZE=0
+# SCRATCH #
