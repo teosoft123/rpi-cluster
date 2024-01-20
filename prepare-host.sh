79b4ab1123f0e1704da3b0f01064a4be8cad709a
Oleg Tsvinev <otsvinev@bitglass.com>
2024-01-19 19:39:55 -0800

    reimplemented adding chroups

--- prepare-host.sh
+++ prepare-host.sh
@@ -19,17 +19,30 @@ sudo sed -iE 's/CONF_SWAPSIZE=.*/CONF_SWAPSIZE=0/g' /etc/dphys-swapfile
 
 echo "Enabling required cgroups only if they're not already present"
 BOOTFILE=/boot/cmdline.txt
-REQUIRED_GROUPS="cgroup_enable=cpuset cgroup_memory=1 cgroup_enable=memory"
-ALL_CGROUPS_PRESENT=$(sudo cat $BOOTFILE |  grep -iE 'cgroup_enable\s*=\s*cpuset' | grep -iE 'cgroup_memory\s*=\s*1' | grep 'cgroup_enable\s*=\s*memory')
+
+(sudo cat $BOOTFILE | grep -iE 'cgroup_enable\s*=\s*cpuset' > /dev/null)
+CGROUP_ENABLE_CPUSET=$?
+(sudo cat $BOOTFILE | grep -iE 'cgroup_memory\s*=\s*1' > /dev/null )
+CGROUP_MEMORY_1=$?
+(sudo cat $BOOTFILE | grep -iE 'cgroup_enable\s*=\s*memory' > /dev/null)
+CGROUP_ENABLE_MEMORY=$?
+
+if [ 1 -eq "$CGROUP_ENABLE_CPUSET" ]; then CGROUP_ENABLE_CPUSET='cgroup_enable=cpuset'; else CGROUP_ENABLE_CPUSET=''; fi
+if [ 1 -eq "$CGROUP_MEMORY_1" ]; then CGROUP_MEMORY_1='cgroup_memory=1'; else CGROUP_MEMORY_1=''; fi
+if [ 1 -eq "$CGROUP_ENABLE_MEMORY" ]; then CGROUP_ENABLE_MEMORY='cgroup_enable=memory'; else CGROUP_ENABLE_MEMORY=''; fi
+
+ADD_CGROUPS="${CGROUP_ENABLE_CPUSET} ${CGROUP_MEMORY_1} ${CGROUP_ENABLE_MEMORY}"
+
 echo "BOOTFILE: ${BOOTFILE}"
-echo "REQUIRED_GROUPS: ${REQUIRED_GROUPS}"
-echo "ALL_CGROUPS_PRESENT: ${ALL_CGROUPS_PRESENT}"
-if [ "" == "$ALL_CGROUPS_PRESENT" ]; then
-  printf "All of required cgroups present, not making any changes: %s\n" "$ALL_CGROUPS_PRESENT"
-else
-  # adding required groups
-  printf "%s\n" "Adding required groups: ${REQUIRED_GROUPS}"
-  sudo sed -i "s/$/ ${REQUIRED_GROUPS}/" $BOOTFILE
-fi
+echo "ADD_CGROUPS: ${ADD_CGROUPS}"
+sudo sed -i "s/$/ ${ADD_CGROUPS}/" $BOOTFILE
+
+#if [ "" == "$ALL_CGROUPS_PRESENT" ]; then
+#  printf "All of required cgroups present, not making any changes: %s\n" "$ALL_CGROUPS_PRESENT"
+#else
+#  # adding required groups
+#  printf "%s\n" "Adding required groups: ${REQUIRED_GROUPS}"
+#  sudo sed -i "s/$/ ${REQUIRED_GROUPS}/" $BOOTFILE
+#fi
 
 # SCRATCH #
