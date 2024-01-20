4445dbc484d62a91b675f386bcc6bbd4b9e3959f
Oleg Tsvinev <otsvinev@bitglass.com>
2024-01-19 20:21:18 -0800

    reimplemented adding chroups

--- prepare-host.sh
+++ prepare-host.sh
@@ -21,33 +21,34 @@ echo "Enabling required cgroups only if they're not already present"
 # Default boot file: console=serial0,115200 console=tty1 root=PARTUUID=25a1dbb8-02 rootfstype=ext4 fsck.repair=yes rootwait
 BOOTFILE=/boot/cmdline.txt
 echo "BOOTFILE: ${BOOTFILE}"
-sudo cat "${BOOTFILE}"
 
-#sudo egrep -i 'cgroup_enable\s*=\s*cpuset' "${BOOTFILE}"
-#CGROUP_ENABLE_CPUSET=$?
-#echo "CGROUP_ENABLE_CPUSET: ${CGROUP_ENABLE_CPUSET}"
-#sudo egrep -i 'cgroup_memory\s*=\s*1' "${BOOTFILE}"
-#CGROUP_MEMORY_1=$?
-#echo "CGROUP_MEMORY_1: ${CGROUP_MEMORY_1}"
-#sudo egrep -i 'cgroup_enable\s*=\s*memory' "${BOOTFILE}"
-#CGROUP_ENABLE_MEMORY=$?
-#echo "CGROUP_ENABLE_MEMORY: ${CGROUP_ENABLE_MEMORY}"
-#echo
-#
-#if [ 1 -eq "$CGROUP_ENABLE_CPUSET" ]; then CGROUP_ENABLE_CPUSET='cgroup_enable=cpuset'; else CGROUP_ENABLE_CPUSET=''; fi
-#if [ 1 -eq "$CGROUP_MEMORY_1" ]; then CGROUP_MEMORY_1='cgroup_memory=1'; else CGROUP_MEMORY_1=''; fi
-#if [ 1 -eq "$CGROUP_ENABLE_MEMORY" ]; then CGROUP_ENABLE_MEMORY='cgroup_enable=memory'; else CGROUP_ENABLE_MEMORY=''; fi
-#
-#echo "CGROUP_ENABLE_CPUSET: ${CGROUP_ENABLE_CPUSET}"
-#echo "CGROUP_MEMORY_1: ${CGROUP_MEMORY_1}"
-#echo "CGROUP_ENABLE_MEMORY: ${CGROUP_ENABLE_MEMORY}"
-#echo
-#
-#ADD_CGROUPS="${CGROUP_ENABLE_CPUSET} ${CGROUP_MEMORY_1} ${CGROUP_ENABLE_MEMORY}"
-#
-#echo "BOOTFILE: ${BOOTFILE}"
-#echo "ADD_CGROUPS: ${ADD_CGROUPS}"
-#sudo sed -i "s/$/ ${ADD_CGROUPS}/" $BOOTFILE
+set +e
+(sudo egrep -i 'cgroup_enable\s*=\s*cpuset' "${BOOTFILE}")
+CGROUP_ENABLE_CPUSET=$?
+echo "CGROUP_ENABLE_CPUSET: ${CGROUP_ENABLE_CPUSET}"
+(sudo egrep -i 'cgroup_memory\s*=\s*1' "${BOOTFILE}")
+CGROUP_MEMORY_1=$?
+echo "CGROUP_MEMORY_1: ${CGROUP_MEMORY_1}"
+(sudo egrep -i 'cgroup_enable\s*=\s*memory' "${BOOTFILE}")
+CGROUP_ENABLE_MEMORY=$?
+echo "CGROUP_ENABLE_MEMORY: ${CGROUP_ENABLE_MEMORY}"
+set -e
+echo
+
+if [ 1 -eq "$CGROUP_ENABLE_CPUSET" ]; then CGROUP_ENABLE_CPUSET='cgroup_enable=cpuset'; else CGROUP_ENABLE_CPUSET=''; fi
+if [ 1 -eq "$CGROUP_MEMORY_1" ]; then CGROUP_MEMORY_1='cgroup_memory=1'; else CGROUP_MEMORY_1=''; fi
+if [ 1 -eq "$CGROUP_ENABLE_MEMORY" ]; then CGROUP_ENABLE_MEMORY='cgroup_enable=memory'; else CGROUP_ENABLE_MEMORY=''; fi
+
+echo "CGROUP_ENABLE_CPUSET: ${CGROUP_ENABLE_CPUSET}"
+echo "CGROUP_MEMORY_1: ${CGROUP_MEMORY_1}"
+echo "CGROUP_ENABLE_MEMORY: ${CGROUP_ENABLE_MEMORY}"
+echo
+
+ADD_CGROUPS="${CGROUP_ENABLE_CPUSET} ${CGROUP_MEMORY_1} ${CGROUP_ENABLE_MEMORY}"
+
+echo "BOOTFILE: ${BOOTFILE}"
+echo "ADD_CGROUPS: ${ADD_CGROUPS}"
+sudo sed -i "s/$/ ${ADD_CGROUPS}/" $BOOTFILE
 
 #if [ "" == "$ALL_CGROUPS_PRESENT" ]; then
 #  printf "All of required cgroups present, not making any changes: %s\n" "$ALL_CGROUPS_PRESENT"
