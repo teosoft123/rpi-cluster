d26d0525bd2b0198c7768d2e7a100342df3c057c
Oleg Tsvinev <otsvinev@bitglass.com>
2024-01-19 17:20:47 -0800

    More logging

--- prepare-host.sh
+++ prepare-host.sh
@@ -13,11 +13,11 @@ echo iptables-persistent iptables-persistent/autosave_v4 boolean true | sudo deb
 echo iptables-persistent iptables-persistent/autosave_v6 boolean true | sudo debconf-set-selections
 sudo apt-get install iptables-persistent -y
 
-# turn the swap off immediately and permanently
+echo "Turning the swap off immediately and permanently"
 sudo swapoff -a
 sudo sed -iE 's/CONF_SWAPSIZE=.*/CONF_SWAPSIZE=0/g' /etc/dphys-swapfile
 
-# enable required cgroups only if they're not already present
+echo "Enabling required cgroups only if they're not already present"
 BOOTFILE=/boot/cmdline.txt
 REQUIRED_GROUPS="cgroup_enable=cpuset cgroup_memory=1 cgroup_enable=memory"
 ALL_CGROUPS_PRESENT=$(sudo cat $BOOTFILE |  grep -iE 'cgroup_enable\s*=\s*cpuset' | grep -iE 'cgroup_memory\s*=\s*1' | grep 'cgroup_enable\s*=\s*memory')
