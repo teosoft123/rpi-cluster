e968e8be8fa02a9488f7d7bf090dca1ccec8a149
Oleg Tsvinev <otsvinev@bitglass.com>
2024-01-19 20:31:36 -0800

    reimplemented adding chroups

--- prepare-host.sh
+++ prepare-host.sh
@@ -21,16 +21,16 @@ echo "Enabling required cgroups only if they're not already present"
 # Default boot file: console=serial0,115200 console=tty1 root=PARTUUID=25a1dbb8-02 rootfstype=ext4 fsck.repair=yes rootwait
 BOOTFILE=/boot/cmdline.txt
 
-printf "%s\n" "Original Boot command line:"
+echo "Original Boot command line:"
 sudo cat "${BOOTFILE}"
 echo
 
 set +e
-(sudo egrep -i 'cgroup_enable\s*=\s*cpuset' "${BOOTFILE}")
+(sudo egrep -i 'cgroup_enable\s*=\s*cpuset' "${BOOTFILE}" > /dev/null)
 CGROUP_ENABLE_CPUSET=$?
-(sudo egrep -i 'cgroup_memory\s*=\s*1' "${BOOTFILE}")
+(sudo egrep -i 'cgroup_memory\s*=\s*1' "${BOOTFILE}" > /dev/null)
 CGROUP_MEMORY_1=$?
-(sudo egrep -i 'cgroup_enable\s*=\s*memory' "${BOOTFILE}")
+(sudo egrep -i 'cgroup_enable\s*=\s*memory' "${BOOTFILE}" > /dev/null)
 CGROUP_ENABLE_MEMORY=$?
 set -e
 echo
@@ -41,10 +41,10 @@ if [ 1 -eq "$CGROUP_ENABLE_MEMORY" ]; then CGROUP_ENABLE_MEMORY='cgroup_enable=m
 
 ADD_CGROUPS="${CGROUP_ENABLE_CPUSET} ${CGROUP_MEMORY_1} ${CGROUP_ENABLE_MEMORY}"
 
-printf "%s\n" "Adding required groups: ${ADD_CGROUPS}"
+echo "Adding required groups: ${ADD_CGROUPS}"
 sudo sed -i "s/$/ ${ADD_CGROUPS}/" $BOOTFILE
 
-printf "%s\n" "Final Boot command line:"
+echo "Final Boot command line:"
 sudo cat "${BOOTFILE}"
 echo
 
