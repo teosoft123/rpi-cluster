f73c3fcf09e5fc67a998290ff204681d4e271cd5
Oleg Tsvinev <otsvinev@bitglass.com>
2024-01-21 15:17:45 -0800

    Added Usage for RPi basic command line monitor reporting script

--- monitor.sh
+++ monitor.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 ## This is a simple CPU/GPU/SSD temperature monitor
 # Requires pcregrep: sudo apt install pcregrep -y
+# Usage: download script to a target system, or just run:
+# curl -sL https://raw.githubusercontent.com/teosoft123/rpi-cluster/main/monitor.sh | bash -
 CORE_TEMP=$(vcgencmd measure_temp | pcregrep -o1 "temp=(.*)")
 SSD_TEMP=$(sudo sudo smartctl -a /dev/sda | pcregrep -o1 "Temperature:\s*(\d*)")
 printf "core temperature:\t%s\n" "$CORE_TEMP"
