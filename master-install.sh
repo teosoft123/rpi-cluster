56d7a2ec583aca317608e2ff9fe2f4c718874cbe
Oleg Tsvinev <otsvinev@bitglass.com>
2024-01-23 17:15:17 -0800

    Getting latest stable k3s

--- master-install.sh
+++ master-install.sh
@@ -15,7 +15,7 @@ curl --no-progress-meter https://get.k3s.io -o $K_INSTALL_SCRIPT
 chmod +x $K_INSTALL_SCRIPT
 
 # If these variables are defined before running script, the defined values will be used
-export INSTALL_K3S_VERSION=v1.27.9+k3s1  # ${INSTALL_K3S_VERSION:+v1.27.9+k3s1}
+export INSTALL_K3S_VERSION=v1.28.5+k3s1  # ${INSTALL_K3S_VERSION:+v1.28.5+k3s1}
 export INSTALL_K3S_NAME=rpi # ${INSTALL_K3S_NAME:+rpi}
 
 RUN_COMMAND="$K_INSTALL_SCRIPT --disable=traefik --flannel-backend=host-gw --bind-address=$K_MASTER_IP --advertise-address=$K_MASTER_IP --node-ip=$K_MASTER_IP $K_TLS_SANS --cluster-init"
